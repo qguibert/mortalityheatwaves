0ca05141adaec35ed052ba66f814dbf7e0b81d53
Quentin Guibert <q.guibert@hotmail.fr>
2025-01-20 17:33:17 +0100

    revision: update figures.

--- functions/summary_forecast_temp_effect.R
+++ functions/summary_forecast_temp_effect.R
@@ -1,65 +1,62 @@
 ########################################
 #' summary_forecast_temp_effect
 ########################################
-summary_forecast_temp_effect <- function(afftab, dec_weight)
+summary_forecast_temp_effect <- function(afftab, dec_weight, var)
 {
   sub_palette <- c("black", "blue", "red", "lightblue", "orange")
 
-
-  # Modify attrib_frac to be homogeneous as the attributable fraction
-  afftab$attrib_frac <- afftab$attrib_frac / (1 + afftab$attrib_frac)
-
   # Aggregate results
-    temp <- afftab %>%
-      ungroup() %>%
-      left_join(dec_weight, by = c("age_bk", "gender")) %>%
-      dplyr::filter(temp_effect %in% c("all_effect",
-                                       "moderate_hot_effect",
-                                       "moderate_cold_effect",
-                                       "extr_hot_effect","extr_cold_effect")) %>%
-      mutate(temp_effect = fct_recode(temp_effect,
-                                      "All effects" = "all_effect",
-                                      "Extreme cold" = "extr_cold_effect",
-                                      "Extreme hot" = "extr_hot_effect",
-                                      "Moderate cold" = "moderate_cold_effect",
-                                      "Moderate hot" = "moderate_hot_effect")) %>%
-      mutate(rcp = fct_recode(rcp,
-                              "RCP 2.6" = "2.6",
-                              "RCP 4.5" = "4.5",
-                              "RCP 8.5" = "8.5")) %>%
-      group_by(traj_clim, rcp, temp_effect, year, sim) %>%
-      summarise(attrib_frac = sum(attrib_frac * w) * 100) %>%
-      ungroup() %>%
-      group_by(rcp, temp_effect, year) %>%
-      summarise(xsmean = mean(attrib_frac),
-                xsmax = quantile(attrib_frac, 0.95),
-                xsmin = max(0,quantile(attrib_frac, 0.05)))
+  temp <- afftab %>%
+    mutate(attrib_frac = !!sym(var) /(1 + !!sym(var)) ) %>%
+    ungroup() %>%
+    left_join(dec_weight, by = c("age_bk", "gender")) %>%
+    dplyr::filter(temp_effect %in% c("all_effect",
+                                     "moderate_hot_effect",
+                                     "moderate_cold_effect",
+                                     "extr_hot_effect","extr_cold_effect")) %>%
+    mutate(temp_effect = fct_recode(temp_effect,
+                                    "All effects" = "all_effect",
+                                    "Extreme cold" = "extr_cold_effect",
+                                    "Extreme hot" = "extr_hot_effect",
+                                    "Moderate cold" = "moderate_cold_effect",
+                                    "Moderate hot" = "moderate_hot_effect")) %>%
+    mutate(rcp = fct_recode(rcp,
+                            "RCP 2.6" = "2.6",
+                            "RCP 4.5" = "4.5",
+                            "RCP 8.5" = "8.5")) %>%
+    group_by(traj_clim, rcp, temp_effect, year, sim) %>%
+    summarise(attrib_frac = sum(attrib_frac * w) * 100) %>%
+    ungroup() %>%
+    group_by(rcp, temp_effect, year) %>%
+    summarise(xsmean = mean(attrib_frac),
+              xsmax = quantile(attrib_frac, 0.95),
+              xsmin = max(0,quantile(attrib_frac, 0.05)))
 
-    # Plot aggregate results
-    res_agg <- temp %>%
-      ggplot(aes(x = year, y = xsmean)) +
-      geom_line(aes(colour = factor(temp_effect))) +
-      geom_ribbon(aes(ymax = xsmax, ymin = xsmin, fill = factor(temp_effect)), alpha = 0.3) +
-      facet_wrap( ~ rcp, scales = "free_y", ncol = 3) +
-      theme(legend.position = "bottom",
-            legend.background = element_rect(fill = "white", color = "black"),
-            strip.background = element_rect(fill = "white", color = NA),
-            strip.text = element_text(color = "black"),
-            panel.border = element_rect(color = "black", fill = NA),
-            text = element_text(size = 12))+
-      labs(x = "Years", y = "Excess mortality (%)", fill = "Temperature effect",
-           colour = "Temperature effect") +
-      scale_colour_manual(values = sub_palette) +
-      scale_fill_manual(values = sub_palette)
+  # Plot aggregate results
+  res_agg <- temp %>%
+    ggplot(aes(x = year, y = xsmean)) +
+    geom_line(aes(colour = factor(temp_effect))) +
+    geom_ribbon(aes(ymax = xsmax, ymin = xsmin, fill = factor(temp_effect)), alpha = 0.3) +
+    facet_wrap( ~ rcp, scales = "free_y", ncol = 3) +
+    theme(legend.position = "bottom",
+          legend.background = element_rect(fill = "white", color = "black"),
+          strip.background = element_rect(fill = "white", color = NA),
+          strip.text = element_text(color = "black"),
+          panel.border = element_rect(color = "black", fill = NA),
+          text = element_text(size = 12))+
+    labs(x = "Years", y = "Excess mortality (%)", fill = "Temperature effect",
+         colour = "Temperature effect") +
+    scale_colour_manual(values = sub_palette) +
+    scale_fill_manual(values = sub_palette)
 
 
 
   # Produce result for each age bucket
   res_bk <- lapply(age_labels, function(i){
     temp <- afftab %>%
+      mutate(attrib_frac = !!sym(var) / (1 + !!sym(var))) %>%
       ungroup() %>%
       dplyr::filter(age_bk == i) %>%
-      # dplyr::filter(sim %in% 1:15) %>%
       dplyr::filter(temp_effect %in% c("all_effect",
                                        "moderate_hot_effect",
                                        "moderate_cold_effect",
